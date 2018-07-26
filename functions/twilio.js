2aca273a0e29ccbfaca879b3586c892c7bd85915
Manoj Yadav <ymanoj821@gmail.com>
2018-07-26 17:53:51 +0530

    Update twilio.js

--- functions/twilio.js
+++ functions/twilio.js
@@ -1,6 +1,6 @@
 const twilio = require('twilio');
 
-const accountSid = 'AC52d1dd632bedc53552803316bcee5b04';
-const authToken = '869d45ad2a58fe8839de7b454625a881';
+const accountSid = '';
+const authToken = '';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
